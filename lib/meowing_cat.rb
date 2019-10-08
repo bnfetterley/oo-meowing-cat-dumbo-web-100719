404ff4c8546019c77b8f7bcb13167bc20aea50fb
Brianna Fetterley <bnfetterley@usfca.edu>
2019-10-08 19:11:17 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,11 +2,17 @@
 class Cat 
   
 maru = Cat.new
-maru.name = "Maru"
- 
-maru.name
-# => "Maru"
- 
-maru.meow
+
+def name(new_name)
+@name = new_name
+end
+
+def name
+  @name
+end
+
+def meow
+  puts "meow!"
+end 
 
 end
